c885b6c829fcb4bd3ea629203aec13d41f2767fa
Krishna Asati <krishnaasati2006@gmail.com>
2026-04-10 09:17:51 +0530

    javascript

---
+++ Variable&DataTypes/index.js
@@ -0,0 +1,45 @@
+// let x = 10;
+// if(true){
+//     let y = 20;
+//     console.log(x);
+//     console.log(y);
+// }
+
+// function solve(){
+//     var age = 25;
+//     console.log(age);
+// }
+// console.log(age);
+// solve();
+
+//<! NOTE!> ....... VARIABLE CRESTION ->
+
+// use can use onstant,varaible and let but problem is that ki variable or constant me kuch na kuch error aate h 
+//  So instead of use cont and var we can use let !!
+
+// let a = 10;
+// a = "babbar";
+// a = true;
+// a = null; 
+
+const a=28;
+console.log(a);
+
+//  const a = 20;
+// console.log(a);  // error not possible
+
+
+
+//  variable naming conventions -> 
+//  1. number,_,$
+//  2. no Space 
+//  3. camelcase use hota h
+//  4. meaningfull 
+//  5. reservered keywords
+
+
+ // DATA TYPES -> 
+ //1.primitive datatype-> Number,string,boolean,undefined,null,bright,symbol
+ //2.non primitive datatype ->  
+
+
