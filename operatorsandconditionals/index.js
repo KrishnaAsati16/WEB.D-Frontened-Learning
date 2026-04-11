e55b54109ec337bd917124ffc61cc2540341f5e5
Krishna Asati <krishnaasati2006@gmail.com>
2026-04-11 12:55:22 +0530

    javascript

---
+++ operatorsandconditionals/index.js
@@ -0,0 +1,29 @@
+// code help me jakar tutorials delh lena js wle
+// arithmetic operators
+
+let a= 10;
+let b =60;
+
+// console.log(a+b);
+
+// let a= 10;
+// let b =60;
+
+// console.log(a-b);
+
+
+// let a= 10;
+// let b =60;
+
+// console.log(a*b);
+
+// 1->INCREMENT AND DECREMENT HOTA H YAAD RAKH NA !!!
+// 2->ASSIGNMENT OPERATORS BHI HOTE H !!
+//EXAMPLE->
+//a = a+10->
+a+=10;
+console.log(a);
+
+//3->compare operater bhi hota h !!
+
+// Easy h code help ki link se theory pdh lena ???
