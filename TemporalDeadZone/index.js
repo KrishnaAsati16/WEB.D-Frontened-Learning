61a7073e4788607f12d7976a6228ac3010deea4d
Krishna Asati <krishnaasati2006@gmail.com>
2026-04-30 22:59:51 +0530

    javascript

---
+++ TemporalDeadZone/index.js
@@ -0,0 +1,60 @@
+// function scope ->
+
+// function sayHello(fullName){
+//     let fullName = "krishna";
+//     console.log("hello duniya",name);
+// }
+// console.log(fullName);
+// sayHello();
+
+
+// const scope ->     // OUTPUT BAHAR PRINT NHI HOTAA HAIN 
+
+// {
+//     const age = 1666
+//     console.log(age)
+// }
+// console.log(age)
+
+
+
+// var scope ->
+
+// console.log(height)
+// {
+//     var height = 100;
+// }                             // OUTPUT PRINT KARBA SAKTE HO UNDER YA BAHAR 
+// console.log(height)
+
+
+
+// global scope ->
+
+// var age = 15;
+// console.log(age)
+// {
+//     console.log(age)
+// }
+
+// if(true)
+//     {
+//     console.log(age) 
+//     }
+
+// for(let i =0; i<=2;i++){
+//     console.log(age)
+// }
+
+// function sayHello(){
+//     console.log("hi",age)
+// }
+// sayHello();
+
+
+ // TEMPORAL DEAD ZONE 
+
+    // console.log(age)
+    // let age = 1888
+    // // console.log(age)
+
+    
