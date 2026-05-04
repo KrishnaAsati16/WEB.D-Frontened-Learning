27fdfe91cc56a21205a0c561b16555de6ee5246e
Krishna Asati <krishnaasati2006@gmail.com>
2026-05-04 20:42:19 +0530

    javascript

---
+++ Inbuild_Object/index.js
@@ -0,0 +1,31 @@
+// Math object
+
+// console.log("krishna");
+
+// console.log(Math.PI)
+
+// console.log(Math.max(60,50,80,40,70,60,799))
+
+// console.log(Math.min(60,50,80,40,70,60,799))
+
+// console.log(Math.round(1.4))
+
+// console.log(Math.ceil(1.4))
+
+// console.log(Math.floor(1.4))
+
+// console.log(Math.abs(-5))
+
+// console.log(Math.random());
+
+// console.log(Math.sqrt(5))
+
+// console.log(Math.pow(2,10))
+
+
+// Date object 
+
+let curr = new Date();
+console.log(curr)
+
+ // online dekh lena easy topic hain 
