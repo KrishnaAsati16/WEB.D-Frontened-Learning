7c7e95592b2f4e6e064504b0a7423c0fd6ef4474
Krishna Asati <krishnaasati2006@gmail.com>
2026-05-10 15:25:42 +0530

    javascript

---
+++ JSDOM/index.js
@@ -0,0 +1,10 @@
+// all work done in console 
+
+// let paraelement = document.getElementById('spara')
+// -> undefined 
+
+// > paraelement
+// -> id vegra print kr dega 
+
+// > paraelement.style.background color ="blue"
+// <- "blue"
