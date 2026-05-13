5522d65320268028a3cab4e9b6d35c30f6fd7219
Krishna Asati <krishnaasati2006@gmail.com>
2026-05-13 23:10:43 +0530

    javascript

---
+++ EventLoop/index.js
@@ -0,0 +1,9 @@
+ // ASYNCHRONOUS code
+ // SYNCHRONOUS (blocking) - waits for each line
+// console.log("Start");
+// // Imagine this takes 3 seconds...
+// console.log("End"); // Must wait ----------------> example
+
+// SYNCHRONOUS code
+
+// MDN remember
